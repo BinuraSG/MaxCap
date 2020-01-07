85cea7e22a0c65518935bb4682a0a9c534d6abc8
BinuraSG <31967543+BinuraSG@users.noreply.github.com>
2020-01-07 15:02:13 -0500

    Relevant Website Files

---
+++ Maxcap HTML/js.js
@@ -0,0 +1,215 @@
+/** Repeat this function for any links with different goto address **/
+/** THIS CODE IS NOT My own, it is taken from an online resource and was slightly edited **/
+
+
+/** Trying to make animation stay after being triggered (ONCE AGAIN NOT MY code)
+
+$("#newget").one("mouseover", function() {
+  $("#newget div").addClass('permahover');
+});
+
+**/
+
+
+
+/** Trial USER / PASS form **/
+/** Failed attempt at slide down navbar :( **/
+$(document).ready(function() {
+   $('div.fadedown').fadeIn(1000).removeClass('fadedown')
+ }
+)
+
+$(document).ready(function () {
+    $('div.hidden').fadeIn(2000).removeClass('hidden');
+});
+
+$(document).ready(function () {
+    $('div.hiddenn').fadeIn(2000).removeClass('hiddenn');
+});
+
+$(document).ready(function () {
+    $('div.hiddennn').fadeIn(3000).removeClass('hiddennn');
+});
+
+$(document).ready(function () {
+    $('div.hiddennnn').fadeIn(3000).removeClass('hiddennnn');
+});
+
+$(document).ready(function () {
+    $('div.hidde').fadeIn(3000).removeClass('hidde');
+});
+
+
+
+
+function ElogIn(){
+var username = document.getElementById("usernamee").value;
+var password = document.getElementById("passworde").value;
+if ( username == "Johnny" && password == "Doey123"){
+alert ("Logged in successfully! Press 'Ok' to be redirected to the user interface");
+window.location = "Employer.html"; 
+return false;
+}
+else{
+alert("Sorry, we couldn't find this account in the database. Please try again.");
+return false;
+}
+}
+
+function logIn(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "John" && password == "Doe123"){
+alert ("Logged in successfully! Press 'Ok' to be redirected to the user interface");
+window.location = "navbar.html"; 
+return false;
+}
+else{
+alert("Sorry, we couldn't find this account in the database. Please try again.");
+return false;
+}
+}
+
+$(document).ready(function(){
+	$(document).keydown(function(event) {
+	    	    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+         
+		event.preventDefault();
+		
+	     }
+	});
+
+	$(window).bind('mousewheel DOMMouseScroll', function (event) {
+	       if (event.ctrlKey == true) {
+           
+		   event.preventDefault();
+	       }
+	});
+});
+    
+/** This function smooth scrolls to a certain location div when a certain div is clicked **/
+    
+
+
+$(document).ready(function(){
+	$('#superscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#fuller");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
+
+
+$(document).ready(function(){
+	$('#smooth').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#scrolldummy");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+});
+
+
+
+
+$(document).ready(function() {
+      var docHeight = $(document).height(),
+      windowHeight = $(window).height(),
+      scrollPercent;
+
+      $(window).scroll(function() {
+        scrollPercent = $(window).scrollTop() / (docHeight - windowHeight) * 100;
+
+        $('.scroll-progress').width(scrollPercent + '%');
+        });
+        });
+
+
+$(document).ready(function(){
+	$('#videoz').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#utube");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
+
+$(document).ready(function(){
+	$('#scrolltoabout').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#aboutpage");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
+
+$(document).ready(function(){
+	$('#homie').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#yeboi");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
+
+
+$(document).ready(function(){
+	$('#Aboutie').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#aboutpage");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
+
+$(document).ready(function(){
+	$('#teamie').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $("#scrollteam");
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1700, function () {
+
+	    });
+	});
+}); 
